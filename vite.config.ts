807c65a6016b9c50edaf6277832f134f4ada31ad
karneish <karneish.sk@gmail.com>
2026-06-10 21:23:32 +0530

    Fixed image paths for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // This is crucial for GitHub Pages
+  base: '/methrix-statpage/', // CHANGE THIS to your exact repo name
 })
